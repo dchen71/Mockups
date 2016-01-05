ad9e2f45a9f8a95ae45e8be7d595a58a5be5fd60
Dan <dchen12812@gmail.com>
2016-01-04 23:32:29 -0800

    added grunt files

--- ristorante con fusion/Gruntfile.js
+++ ristorante con fusion/Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
         }
     },
         useminPrepare: {
-            html: 'app/menu.html',
+            html: 'app/index.html',
             options: {
                 dest: 'dist'
             }
@@ -144,7 +144,7 @@ module.exports = function (grunt) {
               base:{
                    path: 'dist',
                 options: {
-                    index: 'menu.html',
+                    index: 'index.html',
                     maxAge: 300000
                 }
               }
